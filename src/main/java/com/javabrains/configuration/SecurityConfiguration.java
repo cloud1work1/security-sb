d713562d61744db64cd650398ed957ac35999794
unknown <cloud1work1@gmail.com>
2021-01-04 17:40:16 +0530

    For Security using JDBC API

--- src/main/java/com/javabrains/configuration/SecurityConfiguration.java
+++ src/main/java/com/javabrains/configuration/SecurityConfiguration.java
@@ -1,22 +1,38 @@
 package com.javabrains.configuration;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
+	
+	@Autowired
+	DataSource datasource;
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication().withUser("admin").password("admin").roles("ADMIN")
-		.and().withUser("user").password("user").roles("USER");
+		/*auth.inMemoryAuthentication().withUser("admin").password("admin").roles("ADMIN")
+		.and().withUser("user").password("user").roles("USER");*/
+		
+		//use of jdbc authentication
+		auth.jdbcAuthentication()
+			.dataSource(datasource)
+			/*.withDefaultSchema()
+			.withUser(User.withUsername("user").password("user").roles("USER"))
+			.withUser(User.withUsername("admin").password("admin").roles("ADMIN"))*/
+			/*.usersByUsernameQuery("Select username, password, enabled from users where username = ?")
+			.authoritiesByUsernameQuery("Select username, authority from authorities where username = ?")*/
+			;
 	}
 	
 	@Bean
@@ -24,7 +40,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 		return NoOpPasswordEncoder.getInstance();
 	}
 	
-
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
